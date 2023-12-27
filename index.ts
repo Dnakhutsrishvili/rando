4127149ac4c3b8839522ef619189b06cf09333df
davit <dtnakhutsrishvili@gmail.com>
2023-12-27 19:04:22 +0400

    update

--- index.ts
+++ index.ts
@@ -1,101 +1,88 @@
 import "./src/connections/connection";
 import fastify from "fastify";
-import websocket from "@fastify/websocket";
-import userRoute from "./src/users/user.routes";
-import chatRoomRoutes from "./src/chat-rooms/chatroom.routes";
-import messageRoutes from "./src/messages/message.routes";
 import fastifyIO from "fastify-socket.io";
-import cors from "@fastify/cors";
 
 const server = fastify();
-// server.register(fastifyIO);
 
-// server.register(cors, {
-//   origin: "*",
-//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-// });
 server.register(fastifyIO, {
   cors: {
     origin: "*",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   },
 });
-
-
 server.ready().then(() => {
+  const waitingList=[];
+  const userRooms = new Map();
   //@ts-ignore
-  const users = new Map();
-  //@ts-ignore
-
 server.io.on('connection', (socket) => {
-  console.log('User connected');
-
-  // Automatically assign a unique username
-  const username = `User${Date.now()}`;
-  users.set(socket.id, username);
-  socket.emit('assignedUsername', username);
-
-  console.log(users)
-  // Handle one-on-one chat initiation
+  socket.emit('assignedUsername', socket.id);
+  waitingList.push(socket.id)
   socket.on('startChat', () => {
+    socket.emit("clearChat")
 
-    
-    let availableUsers = Array.from(users).filter(([id]) => id !== socket.id);
- 
-
-
-    console.log(availableUsers,"avaliable users")
+    let availableUsers = waitingList.filter((user) => {return user !== socket.id});
+  
     if (availableUsers.length > 0) {
-      const [targetSocketId, targetUsername] = availableUsers[Math.floor(Math.random() * availableUsers.length)];
+      let targetSocketId=availableUsers[Math.floor(Math.random() * availableUsers.length)]
+      let targetUsername=availableUsers[Math.floor(Math.random() * availableUsers.length)]
+
       const room = `${socket.id}-${targetSocketId}`;
 
-      // Notify the users about the chat initiation
-        //@ts-ignore
+      userRooms.set(socket.id, room);
+      userRooms.set(targetSocketId, room);
+          //@ts-ignore
       server.io.to(socket.id).to(targetSocketId).emit('chatStarted', { room, targetUsername });
 
-      // Join the unique room for the two users
+      socket.on('typing', (data)=>{
+        if(data.typing==true){
+          //@ts-ignore
+           socket.emit('display', data)
+       } })
       if (!socket.rooms[room]) {
-        // not in the room
         socket.join(room);
         //@ts-ignore
         server.io.sockets.sockets.get(targetSocketId).join(room);
-      } else {
-        // in the room
-        console.log("room occupied");
-      }
-        //@ts-ignore
+        waitingList.splice(waitingList.indexOf(socket.id), 1)
+        waitingList.splice(waitingList.indexOf(targetSocketId), 1)
 
+        socket.on("chatEnd",()=>{
+          waitingList.push(socket.id)
+          waitingList.push(targetSocketId)
+        })
+      } else {
+        waitingList.push(socket.id)
+        waitingList.push(targetSocketId)
+      }
+      //@ts-ignore
       server.io.sockets.sockets.get(targetSocketId).join(room);
     } else {
-      // Handle if there are no available users for chat
+      socket.emit("clearChat")
       socket.emit('noAvailableUsers');
+      waitingList.indexOf(socket.id) === -1 ? waitingList.push(socket.id) : console.log("This item already exists");
     }
+  
+  });
+  socket.on('privateMessage', ({ room, message,time}) => {
+  //@ts-ignore
+    server.io.to(room).emit('privateMessage', { sender: socket.id, message ,time:time});
   });
 
-  // Handle incoming private messages
-  socket.on('privateMessage', ({ room, message }) => {
-      //@ts-ignore
-
-    server.io.to(room).emit('privateMessage', { sender: users.get(socket.id), message });
-  });
-
-  // Handle disconnection
   socket.on('disconnect', () => {
-    console.log('User disconnected');
-    // Remove user from the users map
-    users.delete(socket.id);
-  });
+  const room = userRooms.get(socket.id);
+
+  if (room) {
+  //@ts-ignore
+    server.io.to(room).emit('chatEnded');
+    userRooms.delete(socket.id);
+    socket.leave(room);
+  }
+  waitingList.splice(waitingList.indexOf(socket.id), 1);
+});
 });
 })
-server.register(userRoute);
-server.register(messageRoutes);
-server.register(chatRoomRoutes);
-
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-
-  console.log(`Server listening at ${address}`);
 });
--- src/chat-rooms/chatroom.routes.ts
+++
@@ -1,8 +0,0 @@
-import { chatRoom } from "./chatRoom.controller";
-
-function chatRoomRoutes(fastify, options, done) {
-  chatRoom(fastify, options);
-  done();
-}
-
-export default chatRoomRoutes;
