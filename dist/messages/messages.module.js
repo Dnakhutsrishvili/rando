3302dcb4ecf8317d8d95b5d2449968bbf12960d5
davit <dtnakhutsrishvili@gmail.com>
2024-01-05 18:30:44 +0400

    moved from typescript to js

--- dist/messages/messages.module.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const messageSchema = new mongoose_1.Schema({
-    message: { type: String, required: true },
-});
-const Message = (0, mongoose_1.model)('Message', messageSchema);
-exports.default = Message;
-//# sourceMappingURL=messages.module.js.map
--- index.js
+++ index.js
@@ -1,46 +1,40 @@
-import "./src/connections/connection";
 import fastify from "fastify";
 import fastifyIO from "fastify-socket.io";
 
-const server = fastify();
 
+const server = fastify();
 server.register(fastifyIO, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    origin: "*"
   },
 });
+
 server.ready().then(() => {
   const waitingList=[];
   const userRooms = new Map();
-  //@ts-ignore
+ 
 server.io.on('connection', (socket) => {
   socket.emit('assignedUsername', socket.id);
   waitingList.push(socket.id)
   socket.on('startChat', () => {
     socket.emit("clearChat")
-
     let availableUsers = waitingList.filter((user) => {return user !== socket.id});
   
     if (availableUsers.length > 0) {
       let targetSocketId=availableUsers[Math.floor(Math.random() * availableUsers.length)]
       let targetUsername=availableUsers[Math.floor(Math.random() * availableUsers.length)]
-
       const room = `${socket.id}-${targetSocketId}`;
 
       userRooms.set(socket.id, room);
       userRooms.set(targetSocketId, room);
-          //@ts-ignore
       server.io.to(socket.id).to(targetSocketId).emit('chatStarted', { room, targetUsername });
 
       socket.on('typing', (data)=>{
         if(data.typing==true){
-          //@ts-ignore
            socket.emit('display', data)
        } })
       if (!socket.rooms[room]) {
         socket.join(room);
-        //@ts-ignore
         server.io.sockets.sockets.get(targetSocketId).join(room);
         waitingList.splice(waitingList.indexOf(socket.id), 1)
         waitingList.splice(waitingList.indexOf(targetSocketId), 1)
@@ -53,7 +47,6 @@ server.io.on('connection', (socket) => {
         waitingList.push(socket.id)
         waitingList.push(targetSocketId)
       }
-      //@ts-ignore
       server.io.sockets.sockets.get(targetSocketId).join(room);
     } else {
       socket.emit("clearChat")
@@ -63,7 +56,6 @@ server.io.on('connection', (socket) => {
   
   });
   socket.on('privateMessage', ({ room, message,time}) => {
-  //@ts-ignore
     server.io.to(room).emit('privateMessage', { sender: socket.id, message ,time:time});
   });
 
@@ -71,7 +63,6 @@ server.io.on('connection', (socket) => {
   const room = userRooms.get(socket.id);
 
   if (room) {
-  //@ts-ignore
     server.io.to(room).emit('chatEnded');
     userRooms.delete(socket.id);
     socket.leave(room);
@@ -80,7 +71,7 @@ server.io.on('connection', (socket) => {
 });
 });
 })
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
